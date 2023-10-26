a081ab936bb4db1c64a3c9d0dfe9a9e3df4552c8
ayushrana1111 <130528995+ayushrana1111@users.noreply.github.com>
2023-10-26 17:15:09 +0530

    Create 2-alphabet.cpp
    This code contains alphabetical pattern using cpp.

---
+++ PATTERN/ALPHABET/2-alphabet.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int row=1;
+    while(row<=n){
+
+        int col=1;
+        while(col<=n){
+
+            char ch= 'A' + row + col - 2;
+            cout<<ch;
+            col++;
+        }
+    cout<<endl;
+    row++;
+    }
+}
+
+// The output is:-
+// A B C
+// B C D 
+// C D E
