b88281cdcb47e6f7bfef180efd2bcff2d5d7e161
ayushrana1111 <130528995+ayushrana1111@users.noreply.github.com>
2023-10-27 09:14:41 +0530

    Create 4-alphabet.cpp
    Triangular alphabetical pattern printing using c++ while loop

---
+++ PATTERN/ALPHABET/4-alphabet.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+int row=1;
+while(row<=n){
+
+    int col=1;
+    while(col<=row){
+
+        char ch= 'A' + row + col - 2;
+        cout<<ch;
+        col++;
+    }
+        cout<<endl;
+        row++;
+}
+}
+
+//The output is:-
+// A 
+// B C 
+// C D E
+// D E F G
